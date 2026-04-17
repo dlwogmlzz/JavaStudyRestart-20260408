c0ddb66f149db99f29298169989a1a0be09594ba
unknown <dpt_eng@CW33A002.dm.dpt-inc.co.jp>
2026-04-17 13:43:05 +0900

    20260417_JavaStudy_절차 지향 직사각형 프로그램 객체 지향으로 변경

--- src/oop1/ex/Account.java
+++ src/oop1/ex/Account.java
@@ -1,4 +1,19 @@
 package oop1.ex;
 
+// 객체 지향 계좌
 public class Account {
+    int balance; // 잔액
+
+    void deposit(int amount) {
+        balance += amount;
+    }
+
+    void withdraw(int amount) {
+        // 잔액이 빼려는 돈보다 크면
+        if (balance >= amount) {
+            balance -= amount;
+        } else {
+            System.out.println("잔액이 부족합니다.");
+        }
+    }
 }
