bf16b28e6a8662ede4130b316a9599c1a3251516
unknown <dpt_eng@CW33A002.dm.dpt-inc.co.jp>
2026-04-17 13:44:07 +0900

    20260417_JavaStudy_객체지향 계좌

--- src/oop1/ex/Rectangle.java
+++ src/oop1/ex/Rectangle.java
@@ -1,4 +1,23 @@
 package oop1.ex;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Rectangle {
+    // 속성(변수)
+    int width;
+    int height;
+
+
+    // 기능(메서드)
+    int calculateArea() {
+        return width * height;
+    }
+
+    int calculatePerimeter() {
+        return 2 * (width * height);
+    }
+
+    boolean isSquare() {
+        return width == height;
+    }
 }
--- src/oop1/ex/RectangleOopMain.java
+++ src/oop1/ex/RectangleOopMain.java
@@ -1,4 +1,19 @@
 package oop1.ex;
-
 public class RectangleOopMain {
+    public static void main(String[] args) {
+
+        Rectangle rec = new Rectangle();
+        rec.width = 5;
+        rec.height = 8;
+
+        int area = rec.calculateArea();
+        System.out.println("넓이: " + area);
+
+        int perimeter = rec.calculatePerimeter();
+        System.out.println("둘레 길이: " + perimeter);
+
+        boolean isSquare = rec.isSquare();
+        System.out.println("정사각형 여부: " + isSquare);
+
+    }
 }
