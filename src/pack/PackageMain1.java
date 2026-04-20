217df96dde6302d94cd4918401107be5d98fcff1
unknown <dpt_eng@CW33A002.dm.dpt-inc.co.jp>
2026-04-20 10:57:26 +0900

    20260420_JavaStudy_패키지 주석 수정

--- src/pack/PackageMain1.java
+++ src/pack/PackageMain1.java
@@ -7,7 +7,7 @@ public class PackageMain1 {
     public static void main(String[] args) {
         //자기와 같은 패키지면 객체 생성하고 바로 쓰면 됌...
         Data data = new Data();
-        //다른 패키지일경우..
+        //다른 패키지일경우인데.... 너무 불편함...
         pack.a.User user = new pack.a.User();
     }
 }
