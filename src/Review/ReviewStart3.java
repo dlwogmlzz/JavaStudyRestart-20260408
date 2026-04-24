f70d23e0a8232257314a4e627bc1fb4d9fee49de
unknown <dpt_eng@CW33A002.dm.dpt-inc.co.jp>
2026-04-24 10:08:47 +0900

    ReviewJava_클래스 도입과 객체 사용에 대해서

--- src/Review/ReviewStart3.java
+++ src/Review/ReviewStart3.java
@@ -1,24 +1,26 @@
-package Review;
+package review;
 
 public class ReviewStart3 {
 
     public static void main(String[] args) {
-        // 학생을 실제 메모리에 만든다, 객체생성
-        Student student1;
-        student1 = new Student();
+        // ReviewStudent객체 생성.
+        ReviewStudent student1; // 1. ReviewStudent타입의 변수 선언..
+        student1 = new ReviewStudent(); // 2. 객체 생성, 참조값을 보관.
+        //.(점,dot)키워드를 사용해서 메모리에 존재하는 객체에 접근(name, age, grade)
         student1.name = "학생1";
-        student1.age = 10;
-        student1.grade = 100;
+        student1.age = 15;
+        student1.grade = 90;
 
-        // 학생2...
-        Student student2 = new Student();
+        ReviewStudent student2 = new ReviewStudent();
         student2.name = "학생2";
-        student2.age = 12;
-        student2.grade = 80;
+        student2.age = 16;
+        student2.grade = 90;
 
-        System.out.println("이름: " + student1.name + ", 나이: " + student1.age + ", 성적: " + student1.grade);
-        System.out.println("이름: " + student2.name + ", 나이: " + student2.age + ", 성적: " + student2.grade);
+        // 객체 생성후 참조값 확인.
+        System.out.println("student1:" + student1);
+        System.out.println("student2:" + student2);
 
-
+        System.out.println("이름:" + student1.name + ", 나이:" + student1.age + ", 성적:" + student1.grade);
+        System.out.println("이름:" + student2.name + ", 나이:" + student2.age + ", 성적:" + student2.grade);
     }
 }
---
+++ src/Review/ReviewStudent.java
@@ -0,0 +1,9 @@
+package review;
+
+// 학생 클래스 정의
+public class ReviewStudent {
+    // 멤버변수, 필드
+    String name;
+    int age;
+    int grade;
+}
