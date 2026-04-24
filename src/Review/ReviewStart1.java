a9ef905acc7b31b94d791137f0926c1e4a11e626
unknown <dpt_eng@CW33A002.dm.dpt-inc.co.jp>
2026-04-24 09:17:28 +0900

    Java-Review복습_개별변수방식&배열방식

--- src/Review/ReviewStart1.java
+++ src/Review/ReviewStart1.java
@@ -1,20 +1,17 @@
-package Review;
+package review;
 
 public class ReviewStart1 {
 
     public static void main(String[] args) {
         String student1Name = "학생1";
-        int student1Age = 10;
-        int student1Grade = 100;
+        int student1Age = 15;
+        int student1Grade = 90;
 
         String student2Name = "학생2";
-        int student2Age = 12;
-        int student2Grade = 80;
+        int student2Age = 17;
+        int student2Grade = 100;
 
-        // 학생 한명을 더 추가하기 위해서는 또 추가하고 번거롭다...
-        // 그래서 배열을 이용한다.
-
-        System.out.println("이름: " + student1Name + ", 나이: " + student1Age + ", 성적: " + student1Grade);
-        System.out.println("이름: " + student2Name + ", 나이: " + student2Age + ", 성적: " + student2Grade);
+        System.out.println("이름:" + student1Name + ", 나이:" + student1Age + ", 성적:" + student1Grade);
+        System.out.println("이름:" + student2Name + ", 나이:" + student2Age + ", 성적:" + student2Grade);
     }
 }
--- src/Review/ReviewStart2.java
+++ src/Review/ReviewStart2.java
@@ -1,19 +1,14 @@
-package Review;
+package review;
 
 public class ReviewStart2 {
 
     public static void main(String[] args) {
-        // 학생 한명을 더 추가하기 위해서는 또 추가하고 번거롭다...
-        // 그래서 배열을 이용한다.
-        // 이것도 매번 배열에 학생을 추가 해줘야 하고, 변경시에 인덱스 위치를 틀리면 에러가 뜬다.
-        String[] studentNames = {"학생1", "학생2", "학생3"};
-        int[] studentAges = {10, 12, 14};
-        int[] studentGrades = {100, 80, 75};
+        String[] studentNames = {"학생1", "학생2"};
+        int[] studentAges = {15, 17};
+        int[] studentGrade = {90, 100};
 
-        // for문으로 출력
-        // 학생수가 늘어 남에 따라 for문으로 늘리기
-        for (int i = 0; i < studentNames.length; i++){
-            System.out.println("이름: " + studentNames[i] + ", 나이: " + studentAges[i] + ", 성적: " + studentGrades[i]);
+        for (int i=0; i < studentNames.length; i++) {
+            System.out.println("이름:" + studentNames[i] + ", 나이:" + studentAges[i] + ", 성적:" + studentGrade[i]);
         }
     }
 }
