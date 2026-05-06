a60dcd72c47c2b5393b01645db31dfcff9f97374
unknown <dpt_eng@CW33A002.dm.dpt-inc.co.jp>
2026-05-06 09:16:05 +0900

    ReviewJava_기본생성자에 대해

---
+++ src/review/construct/MemberDefault.java
@@ -0,0 +1,18 @@
+package review.construct;
+
+public class MemberDefault {
+    String name;
+
+    // 기본 생성자 생성..(자동으로 생성해줌..)
+    /*
+    * 생성자는 반드시 호출되어야 한다.
+    * 생성자가 없으면 기본 생성자가 제공된다.
+    * 생성자가 하나라도 있으면 기본 생성자가 제공되지 않는다.
+    * 이 경우 개발자가 정의한 생성자를 직접 호출해야 한다.
+    * */
+    public MemberDefault() {
+        System.out.println("기본생성자 호출..");
+    }
+
+
+}
