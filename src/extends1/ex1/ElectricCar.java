32d01275b1ddbd91c3b41a07e04cec03cbaee861
unknown <dpt_eng@CW33A002.dm.dpt-inc.co.jp>
2026-05-07 12:49:41 +0900

    202620507_JavaStudy_상속관계에 대해서

---
+++ src/extends1/ex1/ElectricCar.java
@@ -0,0 +1,13 @@
+package extends1.ex1;
+
+// 전기차 기능.
+public class ElectricCar {
+
+    public void move() {
+        System.out.println("전기차를 이동합니다.");
+    }
+
+    public void charge() {
+        System.out.println("충전합니다.");
+    }
+}
---
+++ src/extends1/ex1/GasCar.java
@@ -0,0 +1,13 @@
+package extends1.ex1;
+
+// 휘발류차 기능.
+public class GasCar {
+
+    public void move() {
+        System.out.println("휘발류차를 이동합니다.");
+    }
+
+    public void fillup() {
+        System.out.println("기름을 주유합니다.");
+    }
+}
