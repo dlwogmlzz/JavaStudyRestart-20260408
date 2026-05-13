3c3ec6f7e85b540f5577b95918eed91521722e32
unknown <dpt_eng@CW33A002.dm.dpt-inc.co.jp>
2026-05-13 11:10:50 +0900

    202620513_JavaStudy_OCP원칙

--- src/poly/Car1/CarMain1.java
+++ src/poly/Car1/CarMain1.java
@@ -14,5 +14,9 @@ public class CarMain1 {
         driver.setCar(model3Car);
         driver.drive();
 
+        // 차량 변경(model3 -> newCar)
+        NewCar newCar = new NewCar();
+        driver.setCar(newCar);
+        driver.drive();
     }
 }
---
+++ src/poly/Car1/NewCar.java
@@ -0,0 +1,20 @@
+package poly.Car1;
+
+// OCP원칙 - 새로운 기능 추가(확장)
+public class NewCar implements Car {
+
+    @Override
+    public void startEngine() {
+        System.out.println("NewCar.startEngine");
+    }
+
+    @Override
+    public void offEngine() {
+        System.out.println("NewCar.offEngine");
+    }
+
+    @Override
+    public void pressAccelerator() {
+        System.out.println("NewCar.pressAccelerator");
+    }
+}
